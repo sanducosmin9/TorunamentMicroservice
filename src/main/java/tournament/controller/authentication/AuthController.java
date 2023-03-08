7e4eb4d61288fe5bddc0c7059aae61c46306f21d
sanducosmin9 <56782613+sanducosmin9@users.noreply.github.com>
2023-03-08 12:30:50 +0200

    allow origin

--- src/main/java/tournament/controller/authentication/AuthController.java
+++ src/main/java/tournament/controller/authentication/AuthController.java
@@ -16,7 +16,7 @@ import java.util.stream.Stream;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 @CrossOrigin(
-        origins = {"http://localhost:5179"},
+        origins = {"http://localhost:5173"},
         allowCredentials = "True"
 )
 public class AuthController {
